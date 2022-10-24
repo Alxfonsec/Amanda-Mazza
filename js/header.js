fc62ba73ce02c36f31a9c397e085f0de5c221d91
Alexandre Fonseca <alexandre.ddfonseca@gmail.com>
2022-10-24 17:51:41 -0300

    Alteração header e cover

--- js/header.js
+++ js/header.js
@@ -7,9 +7,11 @@ function myFunction() {
     if (window.pageYOffset > sticky) {
       header.classList.add("sticky");
     header.style.position = 'fixed';
+    header.style.justifyContent = 'center';
     } else {
       header.classList.remove("sticky");
     header.style.position = 'absolute';
+    header.style.justifyContent = 'left';
 
     }
   }
